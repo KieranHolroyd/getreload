ed6db5c835cc0c51fbc079458df84a1487d14713
KieranHolroyd <kieran@vizemedia.ga>
2017-10-14 02:44:51 +0100

    Updates

--- resources/js/reload.js
+++ resources/js/reload.js
@@ -1,2 +1,30 @@
 //Add Ripple Effect To All Buttons With r-btn-ripple.
-$(".r-btn-ripple").ripple();
+$(".r-ripple").ripple();
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            return false;
+          } else {
+            $target.attr('tabindex','-1');
+            $target.focus();
+          };
+        });
+      }
+    }
+  });
